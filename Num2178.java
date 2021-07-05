378943c2b2ce68df909279e28b952e9ddc0ba11f
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-07-06 00:34:38 +0900

    Update Num2178.java

--- Num2178.java
+++ Num2178.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Num2178 {
@@ -8,6 +10,8 @@ public class Num2178 {
     static int N, M;
     static int[][] maze;
     static boolean[][] visited;
+    static int[] dx = {1,-1,0,0};
+    static int[] dy = {0,0,-1,1};
 
     public static void main(String[] args) throws IOException {
 
@@ -28,10 +32,32 @@ public class Num2178 {
             }
         }
 
+        bfs(0,0);
 
+        System.out.println(maze[N-1][M-1]);
 
     }
-    public static void bfs(int N, int M){
 
+    public static void bfs(int a, int b){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {a,b});
+
+        while(!q.isEmpty()){
+            int now[] = q.poll();
+            visited[a][b] = true;
+
+            for(int i = 0; i<4; i++){
+                int x = now[0] + dx[i];
+                int y = now[1] + dy[i];
+
+                if(x >= 0 && y >= 0 && x < N && y < M){
+                    if(maze[x][y] != 0 && !visited[x][y]){
+                        q.offer(new int[] {x,y});
+                        visited[x][y] = true;
+                        maze[x][y] = maze[now[0]][now[1]] + 1;
+                    }
+                }
+            }
+        }
     }
 }
