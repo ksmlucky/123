b96ddfb4568650223745d1c08aa51590b163e6b6
ksmlucky <77111523+ksmlucky@users.noreply.github.com>
2021-08-09 21:20:57 +0900

    백준 10773번 제로

---
+++ Num10773.java
@@ -0,0 +1,39 @@
+// 10773번 제로
+// https://www.acmicpc.net/problem/10773
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Num10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+
+        int K = Integer.parseInt(br.readLine());
+
+        int arr[] = new int[K];
+
+        for (int i = 0; i < K; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < K; i++) {
+            if (arr[i] == 0) {
+                stack.pop();
+            } else {
+                stack.add(arr[i]);
+            }
+        }
+
+        int sum = 0;
+
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+
+        System.out.println(sum);
+    }
+}
+
